2601aeab7a502a319aaf3770ce21b205b5065567
Rajdeep2820 <rajdeepsinghpanwar1@gmail.com>
2025-03-26 19:55:40 +0530

    playbar and js features

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 console.log("Ho jayegaa!!");
 
+let currentSong = new Audio();
+
+function secondsToMinutesSeconds(seconds) {
+    if (isNaN(seconds) || seconds < 0) {
+        return "00:00";
+    }
+
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+}
+
 async function getSongs() {
     let a = await fetch("http://127.0.0.1:5500/songs/")
     let response = await a.text();
@@ -16,38 +32,69 @@ async function getSongs() {
     }
     return songs;
 }
-
+const playMusic = (track , pause=false)=>{
+    // let audio = new Audio("/songs/" + track);
+    currentSong.src = "/songs/" + track;
+    if(!pause){
+    currentSong.play();
+    play.src = "pause.svg";
+    }
+    document.querySelector(".songinfo").innerHTML =  decodeURI(track);
+    document.querySelector(".startTime").innerHTML = "00:00";
+    document.querySelector(".endTime").innerHTML = "00:00";
+}
 async function main() {
-    let songs = await getSongs();
-    console.log(songs);
 
+    let songs = await getSongs();
+    playMusic(songs[0] , true);
+
+    // Show all the songs inthe playlists.
     let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0];
     for (song of songs) {
         songUL.innerHTML = songUL.innerHTML + `<li>
-    <img class="invert" src="music.svg" alt="">
-    <div class="info">
-        <div class="song-name">
-            <div class="scroll-text">
-                ${song.replaceAll("%20", " ")} &nbsp; | &nbsp; 
-                ${song.replaceAll("%20", " ")} &nbsp; | &nbsp; 
-            </div>
-        </div>
-        <div>Rajjoo</div>
-    </div>
-    <div class="playNow">
-        <span>Play Now</span>
-        <img class="invert" src="play.svg" alt="">
-    </div>
-</li>`;
+                            <img class = "invert" src="music.svg" alt="">
+                            <div class="info">
+                                <div class="title" >${song.replaceAll("%20", " ")}</div>
+                                <div>Rajjoo</div>
+                            </div>
+                            <div class="playNow">
+                                <span>Play Now</span>
+                                <img class = "invert" src="play.svg" alt="">
+                            </div>    
+                        </li>`;
     }
-    // Playing the first song
-    var audio = new Audio(songs[0]);
-    audio.play();
 
-    audio.addEventListener("loadeddata", () => {
-        let duration = audio.duration;
-        console.log(audio.duration, audio.currentSrc, audio.currentTime);
+    // attaching an event listener to each song..
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e=>{
+        e.addEventListener("click" , element=>{
+            console.log(e.querySelector(".info").firstElementChild.innerHTML);
+            playMusic(e.querySelector(".title").innerHTML.trim());
+        })   
     })
+     
+    // attach an event litner to play , next and prev songs
+     function togglePlayPause(event) {
+        if (event.type === "click" || (event.type === "keydown" && event.code === "Space")) {
+            event.preventDefault();
+            if (currentSong.paused) {
+                currentSong.play();
+                play.src = "pause.svg";
+            } else {
+                currentSong.pause();
+                play.src = "playbar.svg";
+            }
+        }
+    }
+    // Adding the event listener for both events
+    play.addEventListener("click", togglePlayPause);
+    document.addEventListener("keydown", togglePlayPause);
+
+    currentSong.addEventListener("timeupdate" , ()=>{
+        console.log(currentSong.currentTime , currentSong.duration);
+        document.querySelector(".startTime").innerHTML = `${secondsToMinutesSeconds(currentSong.currentTime)}`;
+        document.querySelector(".endTime").innerHTML = `${secondsToMinutesSeconds(currentSong.duration)}`
+    })
+
 }
 
 main();
---
+++ script2.js
@@ -0,0 +1,11 @@
+
+// play.addEventListener("click" , ()=>{
+    //     if(currentSong.paused){
+    //         currentSong.play();
+    //         play.src = "pause.svg"
+    //     }
+    //     else{
+    //         currentSong.pause();
+    //         play.src = "playbar.svg";
+    //     }
+    // })
